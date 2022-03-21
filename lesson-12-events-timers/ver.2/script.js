939329d1841841c51aca7a2889363931a5f5ef67
mighty <mighty2815736@gmail.com>
2022-03-21 16:36:30 +0300

    version 2

---
+++ lesson-12-events-timers/ver.2/script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000)
+
+var hourHand = document.querySelector('[data-hour-hand');
+var minuteHand = document.querySelector('[data-minute-hand');
+var secondHand = document.querySelector('[data-second-hand');
+
+function setClock() {
+  var currentDate = new Date();
+  var secondRatio = currentDate.getSeconds() / 60;
+  var minutesRatio = (secondRatio + currentDate.getMinutes()) / 60;
+  var hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  setRotation(secondHand, secondRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock();
